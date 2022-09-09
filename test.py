8ba091ce6621461207594537082eea69d9cd0476
chrisgomelan <chrisgomelan@gmail.com>
2022-09-09 09:08:34 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+# name = ["John", "Drake", "Nate", "Brian"]
+# name.append("Marcial")
+# name.sort()
+# print(name)
+
+n = set()
+n.add("Chris")
+n.add(2)
+n.add(1)
+n.add("Chris")
+
+print(n)
+print(f"The set has {len(n)} elemets")
